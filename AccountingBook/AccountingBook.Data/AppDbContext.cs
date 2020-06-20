be84dea09f764fdf50bc72cb9adfc37e30b4b6af
Jawad Shani <jawad.shani@hitachi-eu.com>
2020-06-20 17:49:00 +0200

    db-context added

---
+++ AccountingBook/AccountingBook.Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AccountingBook.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountingBook.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //Setup mappings here
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.Property(e => e.CompanyName)
+                    .IsRequired()
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.ShortName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.CompanyCode).IsRequired().HasMaxLength(5);
+
+            });
+
+
+            #region seed-data
+
+            //Seed Company
+            modelBuilder.Entity<Company>()
+                .HasData(
+                    new Company("hexquote.com", "hexquote", "hex01")
+                );
+
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
--- AccountingBook/AccountingBook.Web/Startup.cs
+++ AccountingBook/AccountingBook.Web/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AccountingBook.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,13 @@ namespace AccountingBook.Web
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
+
+      //Db setup
+      services.AddDbContext<AppDbContext>(options =>
+      {
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+      });
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
