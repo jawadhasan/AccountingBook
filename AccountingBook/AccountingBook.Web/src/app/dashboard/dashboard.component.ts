2213a46d78e67671cd1d421054fadd174feb1521
Jawad Shani <jawad.shani@hitachi-eu.com>
2020-07-14 22:31:51 +0200

    dashboard data

--- AccountingBook/AccountingBook.Web/src/app/dashboard/dashboard.component.ts
+++ AccountingBook/AccountingBook.Web/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'ab-dashboard',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  
+  stats:any=[];
 
-  constructor() { }
+  constructor(public apiService: ApiService) { }
 
   ngOnInit(): void {
+
+    this.stats = [
+      {icon:"fa fa-line-chart",label:"Assets",value:"81,132",colour:"#00ACAC"},
+      {icon:"fa fa-bar-chart",label:"Liabilities",value:"27,835",colour:"#2F8EE5"},
+      {icon:"fa fa-pie-chart",label:"Revenue",value:"7,763",colour:"#6C76AF"},
+      {icon:"fa fa-area-chart",label:"Expenses",value:"4,456",colour:"#EFA64C"},
+      {icon:"fa fa-book",label:"Equity",value:"8,456",colour:"#8BA39C"}
+    ];
   }
 
 }
